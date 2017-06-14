b7ee6c943fedbc59ce5d2b02169d75b6ea0ee301
Bikash <bikash11roy@gmail.com>
2017-06-14 10:48:40 +0600

    Socistic gradient descent implemented

--- src/Network.py
+++ src/Network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 class Network(object):
@@ -12,6 +13,34 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a) + b)
         return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)
+            ]
+
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test
+                ))
+            else:
+                print("Epoch {0} completed".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        pass
+
+    def evaluate(self, test_data):
+        pass
 
 
 def sigmoid(z):
