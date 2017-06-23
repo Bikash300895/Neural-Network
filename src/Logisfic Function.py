07dbb73f2a5eb9de7323aeee8be934f804c25013
Bikash <bikash11roy@gmail.com>
2017-06-24 03:55:08 +0600

    Plot the derivative of the logistic function

--- src/Logisfic Function.py
+++ src/Logisfic Function.py
@@ -19,3 +19,12 @@ plt.show()
 def logistic_derivative(z):
     y = logistic(z)
     return y * (1 - y)
+
+# Plot the derivative of the logistic function
+z = np.linspace(-6,6,100)
+plt.plot(z, logistic_derivative(z), 'r-')
+plt.xlabel('$z$', fontsize=15)
+plt.ylabel('$\\frac{\\partial \\sigma(z)}{\\partial z}$', fontsize=15)
+plt.title('derivative of the logistic function')
+plt.grid()
+plt.show()
