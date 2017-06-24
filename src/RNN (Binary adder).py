e15196353eda428d962fae25518cce31642043fb
Bikash <bikash11roy@gmail.com>
2017-06-25 02:59:22 +0600

    Initializing newral network weight

--- src/RNN (Binary adder).py
+++ src/RNN (Binary adder).py
@@ -23,3 +23,19 @@ binary = np.unpackbits(np.array([range(largest_number)], dtype=np.uint8).T, axis
 for i in range(largest_number):
     int2binary[i] = binary[i]
 
+# input variable
+alpha = 0.1         # Learning rate
+input_dim = 2       # we are adding two input
+hidden_dim = 16
+output_dim = 1
+
+
+# initialize newral network weights
+synapse_0 = 2 * np.random.random((input_dim, hidden_dim))
+synapse_1 = 2 * np.random.random((hidden_dim, output_dim))
+synapse_h = 2 * np.random.random((hidden_dim, hidden_dim))
+
+synapse_0_update = np.zeros_like(synapse_0)
+synapse_1_update = np.zeros_like(synapse_1)
+synapse_h_update = np.zeros_like(synapse_h)
+
