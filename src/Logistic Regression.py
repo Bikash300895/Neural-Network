c92e9429bba0969a38064657b25579ef30341c93
Bikash <bikash11roy@gmail.com>
2017-06-24 22:30:34 +0600

    Define logistic functions

--- src/Logistic Regression.py
+++ src/Logistic Regression.py
@@ -1,6 +1,7 @@
 # Python imports
 import numpy as np
-np.seterr(all='ignore') # ignore numpy warning line multiplication of inf
+
+np.seterr(all='ignore')  # ignore numpy warning line multiplication of inf
 import matplotlib.pyplot as plt
 from matplotlib.colors import colorConverter, ListedColormap
 from matplotlib import cm
@@ -9,9 +10,9 @@ np.random.seed(seed=1)
 
 """Define and generate sample"""
 nb_of_samples_per_class = 20
-red_mean = [-1, 0]      # The mean of red class
-blue_mean = [1, 0]      # The mean of blue class
-std_dev = 1.2           # Standard deviation of both classes
+red_mean = [-1, 0]  # The mean of red class
+blue_mean = [1, 0]  # The mean of blue class
+std_dev = 1.2  # Standard deviation of both classes
 
 # generate sample from both classes
 x_red = np.random.rand(nb_of_samples_per_class, 2) * std_dev + red_mean
@@ -19,11 +20,11 @@ x_blue = np.random.rand(nb_of_samples_per_class, 2) * std_dev + blue_mean
 
 # Merge samples in set of input variables x, and corresponding set of output variables t
 X = np.vstack((x_red, x_blue))
-t = np.vstack((np.zeros((nb_of_samples_per_class,1)), np.ones((nb_of_samples_per_class,1))))
+t = np.vstack((np.zeros((nb_of_samples_per_class, 1)), np.ones((nb_of_samples_per_class, 1))))
 
 # Plot both classes on the x1, x2 plane
-plt.plot(x_red[:,0], x_red[:,1], 'ro', label='class red')
-plt.plot(x_blue[:,0], x_blue[:,1], 'bo', label='class blue')
+plt.plot(x_red[:, 0], x_red[:, 1], 'ro', label='class red')
+plt.plot(x_blue[:, 0], x_blue[:, 1], 'bo', label='class blue')
 plt.grid()
 plt.legend(loc=2)
 plt.xlabel('$x_1$', fontsize=15)
@@ -31,3 +32,25 @@ plt.ylabel('$x_2$', fontsize=15)
 plt.axis([-4, 4, -4, 4])
 plt.title('red vs. blue classes in the input space')
 plt.show()
+
+
+"""Define functions"""
+
+
+# Define logistic function
+def logistic(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def nn(x, w):
+    return logistic(x.dot(w.T))
+
+
+def nn_predict(x, w):
+    return np.around(nn(x, w))
+
+
+def cost(y, t):
+    return - np.sum(np.multiply(t, np.log(y)) + np.multiply((1-t), np.log(1-y)))
+
+
