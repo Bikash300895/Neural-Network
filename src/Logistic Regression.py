866d8e58de961b327678831ad1da5fb4412e3493
Bikash <bikash11roy@gmail.com>
2017-06-24 15:14:38 +0600

    Logistic regression

---
+++ src/Logistic Regression.py
@@ -0,0 +1,12 @@
+# Python imports
+import numpy as np
+np.seterr(all='ignore') # ignore numpy warning line multiplication of inf
+import matplotlib.pyplot as plt
+from matplotlib.colors import colorConverter, ListedColormap
+from matplotlib import cm
+
+np.random.seed(seed=1)
+
+"""Define and generate sample"""
+
+
