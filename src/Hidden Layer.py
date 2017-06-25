23d2351f51ad51d17bc8ad2d750c0d344f0eae5a
Bikash <bikash11roy@gmail.com>
2017-06-25 17:44:26 +0600

    Plot the samples from both classes as lines on a 1D space

--- src/Hidden Layer.py
+++ src/Hidden Layer.py
@@ -23,6 +23,19 @@ x_red_right = np.random.randn(int(number_of_samples_per_class/2), 1) * std_dev +
 
 # Marge samples in set of input variables x, and corresponding set of output variables t
 x = np.vstack((x_blue, x_red_left, x_red_right))
-t = np.vstack((np.ones((x_blue.shape[0], 1)),
-               np.zeros((x_red_left.shape[0], 1)),
-               np.zeros((x_red_right[0], 1))))
+t = np.vstack((np.ones((x_blue.shape[0],1)), 
+               np.zeros((x_red_left.shape[0],1)), 
+               np.zeros((x_red_right.shape[0], 1))))
+
+# Plot the samples from both classes as lines on a 1D space
+plt.figure(figsize=(8, 0.5))
+plt.xlim(-3, 3)
+plt.ylim(-1,1)
+# plot samples
+plt.plot(x_blue, np.zeros_like(x_blue), 'b|', ms=30)
+plt.plot(x_red_left, np.zeros_like(x_red_left), 'r|', ms = 30) 
+plt.plot(x_red_right, np.zeros_like(x_red_right), 'r|', ms = 30) 
+plt.gca().axes.get_yaxis().set_visible(False)
+plt.title('Input samples from the blue and red class')
+plt.xlabel('$x$', fontsize=15)
+plt.show()
