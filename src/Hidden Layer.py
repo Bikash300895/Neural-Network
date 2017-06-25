eb4884a14ca3d960af99097a92517a9d55030f56
Bikash <bikash11roy@gmail.com>
2017-06-25 17:09:49 +0600

    Implementing a hidden layer

---
+++ src/Hidden Layer.py
@@ -0,0 +1,8 @@
+# python import
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import colorConverter, ListedColormap
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm       # ColorMaps
+
+np.random.seed(seed=1)
