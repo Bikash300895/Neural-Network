5dc91ef744c6556217b1dd2add0b6f7373737fa7
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 01:59:42 +0600

    backword prop
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/One hidden layer network/Classification with one hidden layer.py
+++ DeepLearning/One hidden layer network/Classification with one hidden layer.py
@@ -67,9 +67,31 @@ def compute_cost(A2, Y, parameters):
     return cost
 
 
+""" Part 5 - Back Propagetaion """
+def backward_propagation(parameters, cache, X, Y):
+    m = X.shape[1]
+    
 
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
 
+    A1 = cache["A1"]
+    A2 = cache["A2"]
 
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T) / m
+    db2 = 1/m*(np.sum(dZ2, axis=1, keepdims=True))
+    dZ1 = np.dot(W2.T, dZ2) *  (1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T)/m
+    db1 = 1/m*(np.sum(dZ1, axis=1, keepdims=True))
+
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
 
 
 
