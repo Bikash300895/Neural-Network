16f58735131bd4eac9ba036d2002d1dca88514ea
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 00:19:05 +0600

    defining layer size
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/One hidden layer network/Classification with one hidden layer.py
+++ DeepLearning/One hidden layer network/Classification with one hidden layer.py
@@ -9,9 +9,19 @@ import sklearn.linear_model
 from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 np.random.seed(1) # set a seed so that the results are consistent
 
+""" Part 2 - Data preprocessing """
 # Loading the data
 X, Y = load_planar_dataset()
 
 # viasualize the data
 plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
+
+""" Part 3 - Defining NN and helper function """
+def layer_size(X, Y):
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    
+    return n_x, n_h, n_y
+
 
