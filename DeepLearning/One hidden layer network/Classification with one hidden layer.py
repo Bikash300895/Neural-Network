36e575f275ee0b543464089dfa195cac2fd0cca7
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 02:25:24 +0600

    predicting
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/One hidden layer network/Classification with one hidden layer.py
+++ DeepLearning/One hidden layer network/Classification with one hidden layer.py
@@ -141,7 +141,7 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
         grads = backward_propagation(parameters, cache, X, Y)
  
         # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
-        parameters = update_parameters(parameters, grads, learning_rate=0.1)
+        parameters = update_parameters(parameters, grads, learning_rate=1.2)
         
         ### END CODE HERE ###
         
@@ -157,6 +157,14 @@ def predict(parameters, X):
     
     return predictions
 
+# Build a model with a n_h-dimensional hidden layer
+parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
 
+# Plot the decision boundary
+plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+plt.title("Decision Boundary for hidden layer size " + str(4))
 
+# Print accuracy
+predictions = predict(parameters, X)
+print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
 
