a68924d0f2cc0d139c9213dd006e71e556a60f13
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 03:57:57 +0600

    initialize deep network parameters
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/Deep Neural Network/Building Blocks for Deep Neural Network.py
+++ DeepLearning/Deep Neural Network/Building Blocks for Deep Neural Network.py
@@ -15,3 +15,30 @@ np.random.seed(1)
 """ Part 2 - Preprocessing data"""
 
 """ Part 3 - Initializing NN """
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def initialize_parameters_deep(layer_dims):
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+        
+    return parameters
