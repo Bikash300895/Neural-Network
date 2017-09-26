6197d67d9d0cb21652e9e5c48c2c97ada69c8602
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 00:19:29 +0600

    initializing parameters
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/One hidden layer network/Classification with one hidden layer.py
+++ DeepLearning/One hidden layer network/Classification with one hidden layer.py
@@ -25,3 +25,15 @@ def layer_size(X, Y):
     return n_x, n_h, n_y
 
 
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
