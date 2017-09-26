c7c8164baa47ed5cec7e72b56d582255b581c10d
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-27 00:27:17 +0600

    Forword propagation calcualtion
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- DeepLearning/One hidden layer network/Classification with one hidden layer.py
+++ DeepLearning/One hidden layer network/Classification with one hidden layer.py
@@ -37,3 +37,32 @@ def initialize_parameters(n_x, n_h, n_y):
                   "b2": b2}
     
     return parameters
+
+""" Part 4 - Forword propagation """
+def forward_propagation(X, parameters):
+    # Retriving the parameters
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    # Forword propagation calcualtion
+    Z1 = np.dot(W1,X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+
+
+
+
+
+
+
