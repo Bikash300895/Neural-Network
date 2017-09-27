1a7ad9e64d712fdefe944767a6c6bbaa358bc26a
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-28 04:31:57 +0600

    importing data
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Improving Deep Neural Network/Regularization/Regularization.py
+++ Improving Deep Neural Network/Regularization/Regularization.py
@@ -13,3 +13,8 @@ from testCases import *
 plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
+
+# Loading data
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
+
