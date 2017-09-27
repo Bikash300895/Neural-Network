9b4e8ed1c9e06b6a77ccefb0958bf46fb324dc91
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-28 04:16:10 +0600

    random initialization
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Improving Deep Neural Network/Regularization/Initialization.py
+++ Improving Deep Neural Network/Regularization/Initialization.py
@@ -42,6 +42,37 @@ def initialize_parameters_zeros(layers_dims):
 #print("b1 = " + str(parameters["b1"]))
 #print("W2 = " + str(parameters["W2"]))
 #print("b2 = " + str(parameters["b2"]))
+    
+
+def initialize_parameters_random(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+    
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+    
+    np.random.seed(3)               # This seed makes sure your "random" numbers will be the as ours
+    parameters = {}
+    L = len(layers_dims)            # integer representing the number of layers
+    
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+#parameters = initialize_parameters_random([3, 2, 1])
+#print("W1 = " + str(parameters["W1"]))
+#print("b1 = " + str(parameters["b1"]))
+#print("W2 = " + str(parameters["W2"]))
+#print("b2 = " + str(parameters["b2"]))
 
 
 # Defining the model
@@ -103,3 +134,37 @@ def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True,
     plt.show()
     
     return parameters
+
+
+""" Compering the perforamnce """
+### Zero ininialization
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+### Random inint
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
+
+
