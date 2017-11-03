843744bca14f0a5700c879f9f68c4e23987c793c
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-03 07:06:55 +0600

    defining parameters
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Recurrent Neural Network/rnn_numpy.py
+++ Recurrent Neural Network/rnn_numpy.py
@@ -9,3 +9,8 @@ chars = list(set(data))
 data_size, X_size = len(data), len(chars)
 print('data has %d characters, %d unique' % (data_size, X_size))
 
+# Parameters
+H_size = 100            # Size of hidden layer
+T_steps = 25            # Number of time steps (length of sequence) used for training
+learning_rate = 1e-1
+weight_sd = 0.1         # Standard deviation of weights for initialization
